c3d9e16fc6e733f8bcf34147ae64ff0077f905c3
Anubhav Bansal <anubhavb11@gmail.com>
2022-07-02 23:48:49 +0530

    total

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ function Billing(){
 ]);
 
   const handelInputChange = (e,id,type) =>{
-    console.log(id);
     const cpyItems = [...items];
-    console.log(cpyItems[id][type]);
     cpyItems[id][type] = e.target.value;
+    let item = cpyItems[id];
+    if(type === 'rate'){
+      isNaN(item.qty*item.rate)? item["total"] = 0  : item["total"] = item.qty*item.rate;
+    }
     setItems(cpyItems)
   }
   const createNewItem = () =>{
@@ -22,30 +24,60 @@ function Billing(){
     cpyItems.push({});
     setItems(cpyItems)
   }
+
+  const deleteItem = (id) =>{
+    const cpyItems = [...items];
+    cpyItems.splice(id,1)
+    setItems(cpyItems)
+  }
+
+  const createTotal = () =>{
+    const cpyItems = [...items];
+    let ans =0;
+    // console.log(cpyItems)
+    cpyItems.forEach((item,id)=>{
+      
+      if(!isNaN(ans)){
+        ans = ans + parseInt(item.total);
+      }
+      console.log(ans)
+    });
+    // console.log(ans)
+    cpyItems.push({total: ans});
+    console.log(cpyItems);
+    setItems(cpyItems);
+
+  }
+
+
   return(
     <div>
       <table>
         <tr>
+          <th >Sno</th>
+          <th>Name of the item</th>
+          <th>Description</th>
           <th>Qty</th>
-          <th>Name</th>
           <th>Rate</th>
           <th>Total</th>
         </tr>
        
         {items.map((item,id)=>(
              <tr>
-              <td><input type="number" value={item.qty} onChange={(e) => handelInputChange(e,id,"qty")}/></td> 
+              <td className="ex-small">{id +1}</td>
               <td><input type="text" value={item.name} onChange={(e) => handelInputChange(e,id,"name")}/></td>
-              <td><input type="number" value={item.rate} onChange={(e) => handelInputChange(e,id,"rate")}/></td>
-              <td><input type="text" value={isNaN(item.qty*item.rate)? "" : item.qty*item.rate}/></td>
+              <td><input type="text" value={item.description} onChange={(e) => handelInputChange(e,id,"description")}/></td>
+              <td><input type="number" className="small" value={item.qty} onChange={(e) => handelInputChange(e,id,"qty")}/></td> 
+              <td><input type="number" className="small" value={item.rate} onChange={(e) => handelInputChange(e,id,"rate")}/></td>
+              <td><input type="text" className="small" value={item.total}/></td>
+              <button onClick={() => deleteItem(id)} >Delete</button>
               {/* <input>{item.name}</input>
               <input>{item.rate}</input>
               <input>{item.qty*item.rate}</input> */}
             </tr>
         ))}
           <button onClick={() => createNewItem()}>+</button>
-          
-       
+          <button onClick={() => createTotal()}>Total</button>
       </table>
     </div>
   )
