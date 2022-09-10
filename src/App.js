3700ad998939639bff3966c3a4a813439fe11b4b
Anubhav Bansal <anubhav.bansal@thedatateam.onmicrosoft.com>
2022-09-10 15:38:18 +0530

    reverse order

--- src/App.js
+++ src/App.js
@@ -6,66 +6,77 @@ import Bills from './components/Bills/Bills';
 import { Button } from 'reactstrap';
 import { ToastContainer, toast } from 'react-toastify';
 function App() {
-  const [items,setItems] = useState([
+
+  useEffect(() => {
+    const unloadCallback = (event) => {
+      event.preventDefault();
+      event.returnValue = "";
+      return "";
+    };
+
+    window.addEventListener("beforeunload", unloadCallback);
+    return () => window.removeEventListener("beforeunload", unloadCallback);
+  }, []);
+  const [items, setItems] = useState([
     {
       "name": "",
       "description": '',
       "qty": "",
       "rate": "",
       "amount": "",
-    },{
+    }, {
       name: "",
       description: '',
       qty: "",
       rate: "",
       amount: "",
-    },{
+    }, {
       name: "",
       description: '',
       qty: "",
       rate: "",
       amount: "",
-    },{
+    }, {
       name: "",
       description: '',
       qty: "",
       rate: "",
       amount: "",
-    },{
+    }, {
       name: "",
       description: '',
       qty: "",
       rate: "",
       amount: "",
     }
-]);
+  ]);
 
-const [view,setView] = useState('Bill')
+  const [view, setView] = useState('Bill')
 
-  useEffect(()=>{
+  useEffect(() => {
     const x = JSON.parse(localStorage.getItem('userOrder'));
-    if(x){
-    setItems(x);
+    if (x) {
+      setItems(x);
     }
-  },[])
+  }, [])
 
-  const setBillView = (items) =>{
+  const setBillView = (items) => {
     setItems(items);
 
     setView('Invoice');
-    
+
   }
 
-  const handelInputChange = (e,id,type) =>{
+  const handelInputChange = (e, id, type) => {
     const cpyItems = [...items];
     cpyItems[id][type] = e.target.value;
     let item = cpyItems[id];
-    if(type === 'rate' || type === 'qty'){
-      isNaN(item.qty*item.rate)? item["amount"] = 0  : item["amount"] = item.qty*item.rate;
+    if (type === 'rate' || type === 'qty') {
+      isNaN(item.qty * item.rate) ? item["amount"] = 0 : item["amount"] = item.qty * item.rate;
     }
     setItems(cpyItems)
   }
-  const createNewItem = () =>{
+  const createNewItem = () => {
     const cpyItems = [...items];
     cpyItems.push({
       name: "",
@@ -77,31 +88,31 @@ const [view,setView] = useState('Bill')
     setItems(cpyItems)
   }
 
-  const deleteItem = (id) =>{
+  const deleteItem = (id) => {
     const cpyItems = [...items];
-    cpyItems.splice(id,1);
+    cpyItems.splice(id, 1);
     setItems(cpyItems)
   }
 
-  const createTotal = () =>{
+  const createTotal = () => {
     const cpyItems = [...items];
-    let ans =0;
-    cpyItems.forEach((item,id)=>{
-      if(!isNaN(item.amount)&& item.amount!=null && item.amount !== '' && !isNaN(ans)){
+    let ans = 0;
+    cpyItems.forEach((item, id) => {
+      if (!isNaN(item.amount) && item.amount != null && item.amount !== '' && !isNaN(ans)) {
         console.log(ans)
         ans = ans + parseInt(item.amount);
-       
+
       }
       console.log(ans)
     });
     return ans
   }
 
-  const setlocal = () =>{
+  const setlocal = () => {
     toast.success("Saved !");
     localStorage.setItem('userOrder', JSON.stringify(items))
   }
-  const clearItem = () =>{
+  const clearItem = () => {
     toast.success("Cleared !");
     localStorage.setItem('userOrder', JSON.stringify([
       {
@@ -110,62 +121,62 @@ const [view,setView] = useState('Bill')
         "qty": "",
         "rate": "",
         "amount": "",
-      },{
+      }, {
         name: "",
         description: '',
         qty: "",
         rate: "",
         amount: "",
-      },{
+      }, {
         name: "",
         description: '',
         qty: "",
         rate: "",
         amount: "",
-      },{
+      }, {
         name: "",
         description: '',
         qty: "",
         rate: "",
         amount: "",
-      },{
+      }, {
         name: "",
         description: '',
         qty: "",
         rate: "",
         amount: "",
       }
-  ]))
+    ]))
   }
 
-  const changeView = (master = '') =>{
-    if(master === 'master'){
+  const changeView = (master = '') => {
+    if (master === 'master') {
       setView('AllBills');
       return;
     }
-    if(view === 'Bill'){
-    setView('Invoice')
+    if (view === 'Bill') {
+      setView('Invoice')
     }
-    else{
+    else {
       setView('Bill')
     }
   }
 
   return (
     <div className="App">
-      {view === 'AllBills' &&   <Bills setBillView={setBillView}/>}
-      {view === 'Bill' &&  <Billing  items = {items} handelInputChange={handelInputChange} createNewItem={createNewItem} deleteItem={deleteItem} setlocal={setlocal} createTotal={createTotal} clearItem={clearItem}/>}
+      {view === 'AllBills' && <Bills setBillView={setBillView} />}
+      {view === 'Bill' && <Billing items={items} handelInputChange={handelInputChange} createNewItem={createNewItem} deleteItem={deleteItem} setlocal={setlocal} createTotal={createTotal} clearItem={clearItem} />}
       <Button color="info" className='btn' onClick={() => changeView()} >View</Button>
-      {view === 'Invoice' && <Invoice items = {items} createTotal={createTotal}  />}
+      {view === 'Invoice' && <Invoice items={items} createTotal={createTotal} />}
       <Button onClick={() => changeView("master")}>Master View</Button>
       <ToastContainer
-            position="bottom-center"
-            autoClose={500}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            />
+        position="bottom-center"
+        autoClose={500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+      />
     </div>
   );
 }
--- src/components/Bills/Bills.jsx
+++ src/components/Bills/Bills.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react';
-import { getDatabase, ref, onValue} from "firebase/database";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { getAuth } from "firebase/auth";
 import { Button, Table } from 'reactstrap';
 
 
-function Bills({setBillView}) {
+function Bills({ setBillView }) {
     const [userData, setUserdata] = useState()
     const getUserData = () => {
         const db = getDatabase();
+        const auth = getAuth();
+        console.log(auth)
+        // const userId = auth.currentUser.uid;
+        console.log(db)
         const starCountRef = ref(db, '/');
+        console.log(starCountRef);
         onValue(starCountRef, (snapshot) => {
+            console.log("DE")
             const data = snapshot.val();
             const resultArray = Object.keys(data).map(index => {
                 let person = data[index];
                 return person;
             });
             console.log(resultArray)
-            setUserdata(resultArray);
+            // reverse the result array to get the latest data first
+            setUserdata(resultArray.reverse());
+            // setUserdata(resultArray);
         });
         // let refx = database().ref('userData');
         // console.log(refx)
@@ -28,48 +37,48 @@ function Bills({setBillView}) {
         console.log('DATA RETRIEVED');
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         console.log("Dws")
         getUserData();
-    },[])
-  return (
-    <div>
-        <Table>
-            <thead>
-                <tr>
-                    <th>
-                        BillNo
-                    </th>
-                    <th>
-                        Date Time
-                    </th>
-                    <th>
-                        Name
-                    </th>
-                    <th>
-                        Address
-                    </th>
-                    <th>
-                        PageView
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    userData && userData.map((item,idx)=>(
-                        <tr>
-                            <td>{idx+1}</td>
-                            <td>{item.dateAndTime}</td>
-                            <td>{item.name}</td>
-                            <td>{item.cusAddress}</td>
-                            <td><Button color='info'onClick={() => setBillView(item.items)} >View</Button> </td>
-                        </tr>
-                    ))
-                }
-        </tbody>
-        </Table>        
-    </div>
-  )
+    }, [])
+    return (
+        <div>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>
+                            BillNo
+                        </th>
+                        <th>
+                            Date Time
+                        </th>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Address
+                        </th>
+                        <th>
+                            PageView
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        userData && userData.map((item, idx) => (
+                            <tr>
+                                <td>{idx + 1}</td>
+                                <td>{item.dateAndTime}</td>
+                                <td>{item.name}</td>
+                                <td>{item.cusAddress}</td>
+                                <td><Button color='info' onClick={() => setBillView(item.items)} >View</Button> </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </Table>
+        </div>
+    )
 }
 
 export default Bills;
